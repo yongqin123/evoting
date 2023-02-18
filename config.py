65814b974ca22a7b943fdf251067d21264aa872a
yongqin123 <66666937+yongqin123@users.noreply.github.com>
2023-02-18 17:28:06 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://admin:csci321voting@evoting.chzmr6u2ohlt.ap-southeast-1.rds.amazonaws.com/voting'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://evotingsystem:EmXAdCIbKic5IL6TL9e3@fyp-22-s4-30.cuhxaq43l2oj.ap-southeast-1.rds.amazonaws.com/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
    
